283e83e714209cc255d8860b8a8b11739ca7f775
Damir <imyaminovdamir10@gmail.com>
2025-09-10 22:51:49 +0600

    chore: sort imports

--- lib/features/profile/presentation/widgets/avatar.dart
+++ lib/features/profile/presentation/widgets/avatar.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:auto_route/auto_route.dart';
-import 'package:image_picker/image_picker.dart';
-
 import 'package:test_3/core/presentation/constants/constants.dart';
 import 'package:test_3/core/presentation/utils/utils.dart';
 import 'package:test_3/core/presentation/widgets/app_icon.dart';
